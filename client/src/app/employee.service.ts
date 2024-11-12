857da677b7cc5487aac755da39c7e8dfb88cb348
waiweng84 <65702453+waiweng84@users.noreply.github.com>
2024-11-12 21:25:58 +1100

    update to expose port 4000 and backend url

--- client/src/app/employee.service.ts
+++ client/src/app/employee.service.ts
@@ -6,7 +6,7 @@ import { Employee } from './employee';
   providedIn: 'root'
 })
 export class EmployeeService {
-  private url = 'http://localhost:5200';
+  private url = 'http://mean-stack-backend:5200';
   employees$ = signal<Employee[]>([]);
   employee$ = signal<Employee>({} as Employee);
   
